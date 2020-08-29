1b068a8e989b5274db65aec746441b17896341be
Heverton Sarah <heverton.sarah@gmail.com>
2020-08-29 11:10:15 -0300

    Add general class structures

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection
+{
+    public struct Node
+    {
+
+    }
+
+    public float Cost
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    public Node FromNode
+    {
+        get
+        {
+            return new Node();
+        }
+    }
+
+    public Node ToNode
+    {
+        get
+        {
+            return new Node();
+        }
+    }
+}
---
+++ Assets/Scripts/Grapth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapth
+{
+    public Connection[] Conections
+    {
+        get
+        {
+            return new Connection[0];
+        }
+    }
+}
